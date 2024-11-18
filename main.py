0deb03ee3c44094bfaf14a6324313e7706a14b7d
ethan.aquino <ethan.aquino.258@gmail.com>
2024-11-17 19:44:05 -0800

    in-memory streaming and audio file modification added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth
 from dotenv import load_dotenv
+from io import BytesIO
+from pydub import AudioSegment
 import os
 
 load_dotenv()
@@ -14,27 +16,26 @@ coyote__training_folder_id='293777823612'
 woodhouse_toad_training_folder_id = '293921660447'
 # pacific_chorus_frog__training_folder_id = ''
 
-os.makedirs('training_sets', exist_ok=True)
-
 training_folders = client.folders.get_folder_items(bullfrog_training_folder_id)
-os.makedirs(os.path.join('training_sets', 'bullfrog'), exist_ok=True)
 
 for training_set in training_folders.entries:
     subfolders = client.folders.get_folder_by_id(training_set.id)
-    os.makedirs(os.path.join('training_sets/bullfrog', training_set.name),exist_ok=True)
 
     for item in subfolders.item_collection.entries:
         neg_pos_folders = client.folders.get_folder_items(item.id)
-        os.makedirs(os.path.join(f'training_sets/bullfrog/{training_set.name}', item.name), exist_ok=True)
 
         for audio_file in neg_pos_folders.entries:
             audio_clip = client.downloads.download_file(audio_file.id)
+            audio_buffer = BytesIO()
+            for chunk in audio_clip:
+                audio_buffer.write(chunk)
+            
+            audio_buffer.seek(0)
+            audio = AudioSegment.from_file(audio_buffer)
+            if len(audio) < 5000:
+                total_padding = 5000 - len(audio)
+                padding_start = total_padding // 2
+                padding_end = total_padding - padding_start
 
-            with open(f'training_sets/bullfrog/{training_set.name}/{item.name}/{audio_file.name}', 'bw') as new_file:
-                for chunk in audio_clip:
-                    new_file.write(chunk)
-
-            #this is where the audio padding code should go
-
-folder = client.folders.get_folder_items('293780792480')
-print(folder)
+            new_audio = AudioSegment.silent(duration=padding_start) + audio + AudioSegment.silent(duration=padding_end)
+            new_audio.export(f'{audio_file.name}',format='wav')
