44a2407ba3d7112ff7c4e2b22c384467181fbed4
ethan.aquino <ethan.aquino.258@gmail.com>
2024-11-18 19:55:40 -0800

    uploading functionality

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth
+from box_sdk_gen import BoxClient, BoxDeveloperTokenAuth, CreateFolderParent, UploadFileAttributes, UploadFileAttributesParentField
 from dotenv import load_dotenv
 from io import BytesIO
 from pydub import AudioSegment
@@ -20,10 +20,11 @@ training_folders = client.folders.get_folder_items(bullfrog_training_folder_id)
 
 for training_set in training_folders.entries:
     subfolders = client.folders.get_folder_by_id(training_set.id)
-
+    print(subfolders.item_collection.entries)
     for item in subfolders.item_collection.entries:
         neg_pos_folders = client.folders.get_folder_items(item.id)
-
+        new_folder = client.folders.create_folder(name=f'{item.name}_5_seconds',parent= CreateFolderParent(id=item.id))
+        
         for audio_file in neg_pos_folders.entries:
             audio_clip = client.downloads.download_file(audio_file.id)
             audio_buffer = BytesIO()
@@ -38,4 +39,10 @@ for training_set in training_folders.entries:
                 padding_end = total_padding - padding_start
 
             new_audio = AudioSegment.silent(duration=padding_start) + audio + AudioSegment.silent(duration=padding_end)
-            new_audio.export(f'{audio_file.name}',format='wav')
+            file_to_upload = new_audio.export(f'{audio_file.name}',format='wav')
+            client.uploads.upload_file(
+                UploadFileAttributes(
+                    name=audio_file.name, parent=UploadFileAttributesParentField(id=new_folder.id)
+                ), 
+                file_to_upload
+            )
